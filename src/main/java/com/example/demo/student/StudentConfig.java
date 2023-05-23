cfbc456dced3a176448c4be3cc5a78f254bbd896
Gavin Loo <looweiren@gmail.com>
2023-05-24 00:06:32 +0800

    added POST route

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -16,11 +16,11 @@ public class StudentConfig {
         return args -> {
 
             Student gavin = new Student(
-                    "Gavin", "gavinloo@gmail.com", LocalDate.of(2000, Month.JANUARY, 5), 21
+                    "Gavin", "gavinloo@gmail.com", LocalDate.of(2000, Month.JANUARY, 5)
             );
 
             Student alex = new Student(
-                    "Alex", "alextan@gmail.com", LocalDate.of(2000, Month.APRIL, 16), 21
+                    "Alex", "alextan@gmail.com", LocalDate.of(2000, Month.APRIL, 16)
             );
 
             repository.saveAll(List.of(gavin, alex));
