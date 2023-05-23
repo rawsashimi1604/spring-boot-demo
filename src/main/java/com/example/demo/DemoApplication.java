41255bb15b48d2e742be1283529e6da8967a3a30
Gavin Loo <looweiren@gmail.com>
2023-05-23 22:55:11 +0800

    added database and .env support for DB_USERNAME and DB_PASSWORD

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
-@RestController
+@PropertySources({
+        @PropertySource(value="file:${user.dir}/.env", ignoreResourceNotFound = true)
+})
 public class DemoApplication {
 
     public static void main(String[] args) {
