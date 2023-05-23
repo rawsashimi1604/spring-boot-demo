8add18749074158dac1a52055430381c18ee857d
Gavin Loo <looweiren@gmail.com>
2023-05-23 22:27:48 +0800

    added simple API routes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,27 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String helloWorld() {
+		return "Hello World";
+	}
+
+	@GetMapping("/list")
+	public List<String> helloWorldList() {
+		return List.of("Hello", "World");
 	}
 
 }
